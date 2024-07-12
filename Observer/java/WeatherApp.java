16b56801e2b49efae400bc1cb22b142785e60cf4
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-07-12 16:32:26 +0200

    Refactor WeatherApp to remove TVDisplay observer

--- Observer/java/WeatherApp.java
+++ Observer/java/WeatherApp.java
@@ -8,16 +8,13 @@ public class WeatherApp {
         WeatherStation weatherStation = new WeatherStation();
 
         Observer phoneDisplay = new PhoneDisplay();
-        Observer tvDisplay = new TVDisplay();
 
         weatherStation.addObserver(phoneDisplay);
-        weatherStation.addObserver(tvDisplay);
 
         // Simulating weather change
         weatherStation.setWeather("Sunny");
 
         // Output:
         // Phone Display: Weather updated - Sunny
-        // TV Display: Weather updated - Sunny
     }
 }
