81868edb9b02bf5df98f38a6d0e9757411946ace
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-07-21 11:00:26 +0200

    Changed the Chain folder so that it doesn't give errors with the java package

---
+++ Chain/java/Request.java
@@ -0,0 +1,5 @@
+package Chain.java;
+
+public class Request {
+    
+}
---
+++ Chain/java/SupportHandler.java
@@ -0,0 +1,7 @@
+package Chain.java;
+
+
+public interface SupportHandler {
+    void handleRequest(Request request);
+    void setNextHandler(SupportHandler nextHandler);
+}
