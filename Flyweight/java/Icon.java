b434c6a7acb6f22f4faeddb2dad8cc679a4f6c02
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-07-23 10:22:27 +0200

    Added Java Implementation

---
+++ Flyweight/java/Icon.java
@@ -0,0 +1,6 @@
+package Flyweight.java;
+
+// Flyweight interface
+public interface Icon {
+    void draw(int x, int y);  // Method to draw the icon at given coordinates
+}
---
+++ Flyweight/java/Image.java
@@ -0,0 +1,11 @@
+package Flyweight.java;
+
+public class Image {
+
+    private String filename;
+
+    public Image(String filename) {
+        this.filename = filename;
+    }
+
+}
